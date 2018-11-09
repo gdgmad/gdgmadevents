7387cc7828b5b8f4b06c79f346b801ec1c217011
Thakkar, Nikhil (US - Mumbai) <nikthakkar@deloitte.com>
2018-11-09 12:38:51 +0530

    1. Added engines node to package.json 2. Added mock data

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,9 @@
 import * as functions from 'firebase-functions';
+import { event } from './mock'
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const bot = functions.https.onRequest((request, response) => {
+    response.send(event);
+})
---
+++ functions/src/mock/event.ts
@@ -0,0 +1,25 @@
+export const event = {
+    venue: {
+        address: "Endurance\n124, Shivaji Path, Thane west",
+        map: "123,123"
+    },
+    date: "24th November, 2018",
+    "registration_link": "https://meetup.com",
+    agenda: [{
+        desc: "Hot new talk",
+        slot: "11 - 11:45am",
+        speaker: {
+            name: "New Speaker",
+            photo: "https://freeiconshop.com/wp-content/uploads/edd/android-flat.png",
+            twitter: "@tester"
+        }
+    }, {
+        desc: "",
+        slot: "",
+        speaker: {
+            name: "",
+            photo: "",
+            twitter: ""
+        }
+    }]
+}
