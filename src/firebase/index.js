c8a13a01dcaac58247815632e0ad1c0d8bc1108f
Aniket Kadam <anikadamg@gmail.com>
2018-11-11 19:17:41 +0530

    Moved the firebase configuration to another file so it can be more easily used

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,13 +1,5 @@
 import * as firebase from 'firebase'
-
-const config = {
-    apiKey: "AIzaSyCAY1A2gh4-B11ebfYLojU2_Erug0C-64c",
-    authDomain: "gdgmadevents.firebaseapp.com",
-    databaseURL: "https://gdgmadevents.firebaseio.com",
-    projectId: "gdgmadevents",
-    storageBucket: "gdgmadevents.appspot.com",
-    messagingSenderId: "561574056380"
-}
+import config from './firebaseConfig'
 
 class Firebase {
 
