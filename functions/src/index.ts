fce8a6f1cb37e4098afe271543e686773e9e13e4
nikhil-thakkar <nikhil.thakkar1987@gmail.com>
2018-11-23 20:55:08 +0530

    Deployed the cloud function branch to firebase

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,24 @@
-import * as functions from 'firebase-functions';
-import { event } from './mock'
+import * as functions from 'firebase-functions'
+import * as express from 'express'
+import * as admin from 'firebase-admin'
+import * as cors from 'cors'
+import * as lodash from 'lodash'
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const bot = functions.https.onRequest((request, response) => {
-    response.send(event);
+admin.initializeApp(functions.config())
+admin.firestore().settings({ timestampsInSnapshots: true })
+
+const app = express()
+app.use(cors())
+
+app.get('/', async (req: express.Request, res: express.Response) => {
+    const snapshot = await admin.firestore().collection('events').get()
+    const result = []
+    snapshot.forEach(doc => {
+        const data = doc.data()
+        data.agenda = lodash.values(data.agenda)
+        result.push(data)
+    })
+    res.send(result)
 })
+
+export const event = functions.https.onRequest(app)
--- functions/src/mock/event.ts
+++ functions/src/mock/event.ts
@@ -1,25 +1,41 @@
-export const event = {
-    venue: {
-        address: "Endurance\n124, Shivaji Path, Thane west",
-        map: "123,123"
-    },
-    date: "24th November, 2018",
-    "registration_link": "https://meetup.com",
-    agenda: [{
-        desc: "Hot new talk",
-        slot: "11 - 11:45am",
-        speaker: {
-            name: "New Speaker",
-            photo: "https://freeiconshop.com/wp-content/uploads/edd/android-flat.png",
-            twitter: "@tester"
-        }
-    }, {
-        desc: "",
-        slot: "",
-        speaker: {
-            name: "",
-            photo: "",
-            twitter: ""
-        }
-    }]
-}
+export const data = [
+    {
+        "venue": {
+            "address": "Endurance International Group\nUnit No. 501, 5th Floor, IT Building 3, Nesco IT park,\nNesco Complex, Western Express Highway,\nGoregaon (east), Mumbai-400063",
+            "link": "https://goo.gl/maps/MuPPtLuHVuL2"
+        },
+        "agenda": [
+            {
+                "end": "12:15 pm",
+                "name": "try! Swift",
+                "speaker": {
+                    "twitter": "@_yogeshsingh",
+                    "name": "Yogesh Singh"
+                },
+                "start": "11:45 am"
+            },
+            {
+                "end": "01:15 pm",
+                "name": "PWAMP, the music of AMP+PWA",
+                "speaker": {
+                    "name": "Rajat Chowdhary",
+                    "twitter": "@technorajat"
+                },
+                "start": "12:30 pm"
+            },
+            {
+                "start": "11:00 am",
+                "end": "11:45 am",
+                "name": "We build Empathy, not Just Models",
+                "speaker": {
+                    "twitter": "@Avanish52111547",
+                    "name": "Avani Shah"
+                }
+            }
+        ],
+        "desc": "<p><span style=\"color: rgb(46,62,72);background-color: rgb(246,247,248);font-size: 16px;font-family: Graphik Meetup\", -apple-system, system-ui, Roboto, Helvetica, Arial, sans-serif;\">We're GDG MAD now!</span><br><span style=\"color: rgb(46,62,72);background-color: rgb(246,247,248);font-size: 16px;font-family: Graphik Meetup\", -apple-system, system-ui, Roboto, Helvetica, Arial, sans-serif;\">Join us for our first meetup as a GDG, with three riveting talks, followed by our very first edition of Lets Build, an initiative which we'll tell you more about on the day!</span></p>\n",
+        "name": "GDGMAD November, 2018",
+        "time": 1542997800000,
+        "date": "24-11-2018"
+    }
+]
